c3d4604965f887cb56b513e0eba5eb499dd17b3e
guilhermerodz <guilherme.rodrigues.profissional@gmail.com>
2020-01-04 23:50:37 -0300

    feat: done

---
+++ environment.example.js
@@ -0,0 +1,4 @@
+const ENVIRONMENT = {
+  twitchChannel: 'YOUR_TWITCH_CHANNEL_NAME',
+  twitchApiKey: 'YOUR_TWITCH_API_KEY',
+};
---
+++ options.js
@@ -0,0 +1,8 @@
+const OPTIONS = {
+  messageTimeout: 1000 * 60 * 3,
+  colors: {
+    background: '#121212',
+    userName: '#ff6bb5',
+    text: '#fff',
+  },
+};
